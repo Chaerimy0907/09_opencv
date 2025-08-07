1215bf8eb9981686261f1d8acab2467c9e25dd98
Chaerimy0907 <qkrcofus1026@daum.net>
2025-08-07 15:46:10 +0900

    얼굴 검출 코드

--- src/auto_mosaic.py
+++ src/auto_mosaic.py
@@ -3,29 +3,27 @@
 
 import cv2
 
+# 변수
+rate = 15
+
 # 얼굴과  검출을 위한 케스케이드 분류기 생성 
 face_cascade = cv2.CascadeClassifier('../data/haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('../data/haarcascade_eye.xml')
 
 # 카메라 캡쳐 활성화
 cap = cv2.VideoCapture(0)
 while cap.isOpened():    
-    ret, img = cap.read()  # 프레임 읽기
+    ret, frame = cap.read()  # 프레임 읽기
     if ret:
+        img = frame.copy()
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         # 얼굴 검출    
         faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, \
                                         minNeighbors=5, minSize=(80,80))
-        for(x,y,w,h) in faces:
-            cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255,0),2)
-            roi = gray[y:y+h, x:x+w]
-            # 눈 검출
-            eyes = eye_cascade.detectMultiScale(roi)
-            for i, (ex, ey, ew, eh) in enumerate(eyes):
-                if i >= 2:
-                    break
-                cv2.rectangle(img[y:y+h, x:x+w], (ex,ey), (ex+ew, ey+eh), \
-                                    (255,0,0),2)
+        if len(faces) == 1:
+            (x,y,w,h) = faces[0]
+            # 얼굴 영역 표시
+            cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0),1)
+           
         cv2.imshow('face detect', img)
     else:
         break
